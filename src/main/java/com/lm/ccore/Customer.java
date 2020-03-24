2acb78fba74d9d161f6004f34f3f0ab15963a4b1
jegger <jegger@hsr.ch>
2020-03-24 12:31:39 +0100

    first commit

---
+++ src/main/java/com/lm/ccore/Customer.java
@@ -0,0 +1,157 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
+// Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2020.03.24 um 12:09:06 PM CET 
+//
+
+
+package com.lm.ccore;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java-Klasse für customer complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="customer"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="birthday" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *         &lt;element name="address" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="segment" type="{http://lm.com/ccore}profile"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "customer", propOrder = {
+    "name",
+    "birthday",
+    "address",
+    "segment"
+})
+public class Customer {
+
+    @XmlElement(required = true)
+    protected String name;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar birthday;
+    @XmlElement(required = true)
+    protected String address;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "string")
+    protected Profile segment;
+
+    /**
+     * Ruft den Wert der name-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Legt den Wert der name-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Ruft den Wert der birthday-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * Legt den Wert der birthday-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setBirthday(XMLGregorianCalendar value) {
+        this.birthday = value;
+    }
+
+    /**
+     * Ruft den Wert der address-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Legt den Wert der address-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddress(String value) {
+        this.address = value;
+    }
+
+    /**
+     * Ruft den Wert der segment-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Profile }
+     *     
+     */
+    public Profile getSegment() {
+        return segment;
+    }
+
+    /**
+     * Legt den Wert der segment-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Profile }
+     *     
+     */
+    public void setSegment(Profile value) {
+        this.segment = value;
+    }
+
+}
---
+++ src/main/java/com/lm/ccore/Profile.java
@@ -0,0 +1,60 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
+// Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2020.03.24 um 12:09:06 PM CET 
+//
+
+
+package com.lm.ccore;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für profile.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="profile"&gt;
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *     &lt;enumeration value="n/a"/&gt;
+ *     &lt;enumeration value="normal"/&gt;
+ *     &lt;enumeration value="VIP"/&gt;
+ *   &lt;/restriction&gt;
+ * &lt;/simpleType&gt;
+ * </pre>
+ * 
+ */
+@XmlType(name = "profile")
+@XmlEnum
+public enum Profile {
+
+    @XmlEnumValue("n/a")
+    N_A("n/a"),
+    @XmlEnumValue("normal")
+    NORMAL("normal"),
+    VIP("VIP");
+    private final String value;
+
+    Profile(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static Profile fromValue(String v) {
+        for (Profile c: Profile.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
