b15c2a0c951f0f3e5d3aede61dfa4fc07bfd13a2
jegger <jegger@hsr.ch>
2020-03-25 08:47:36 +0100

    auto commit 1

---
+++ deploy.bat
@@ -0,0 +1,15 @@
+@echo off
+cmd /c az login
+pause
+cmd /c az group create --location westus --name lakeside-rg
+pause
+cmd /c az appservice plan create -g lakeside-rg -n lakeside-service-plan --is-linux
+pause
+cmd /c az webapp create -g lakeside-rg -p lakeside-service-plan -n %1 --runtime "JAVA|8-jre8"
+pause
+git add --all
+pause
+git commit -m"%2"
+pause
+git push origin master
+pause
